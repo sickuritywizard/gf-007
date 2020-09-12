0d9a7ad511e0b0fd017931fd24b97bfcc9b75813
Pradeep Kumar N <46489822+killeroo7@users.noreply.github.com>
2020-09-13 03:22:31 +0530

    Update gf-007.py

--- gf-007.py
+++ gf-007.py
@@ -28,104 +28,59 @@ def banner():
 
 
 def get_arguments():
-	parser = argparse.ArgumentParser("ls * | gf-007")
+	parser = argparse.ArgumentParser("Runs gf in current Directory")
+	parser.add_argument('-w','--whitelist',dest='whitelist',help="Run Gf only on these extensions(js,html,txt)")
+	parser.add_argument('-b','--blacklist',dest='blacklist',help="Exclude gf on these extensions(php,html)")
+	parser.add_argument('-a','--all-files',dest='all_files',action="store_true",help="Run Gf on js,html,etc files")
+	parser.add_argument('-u','--url-files',dest='url_files',help="Run Gf on a Files with URL [Ex: -u file1,file2]")
 	args = parser.parse_args()
+	return args
 
 
-def gfauto():
-	if os.path.exists("gf_results"):
+def gf_all_files():
+
+	try:
+		os.mkdir("gf_results")
+	except FileExistsError:
 		print(colored("[-] gf_results Directory Already Exists","red"))
 		exit(0)
 
-	try:
-		os.mkdir("../gf_results")
-	except FileExistsError:
-		print(colored("[-] gf_results Directory Already Exists in parent Directory(../gf_results)","red"))
-		exit(0)
-
-
-	gf_list=((subprocess.check_output("gf --list",shell=True)).decode()).split("\n")
 	all_files = os.listdir()
 
 	for file in all_files:   #Removing Directories
 		if os.path.isdir(file):
 			all_files.remove(file)
 
-	for file in all_files:   #Removing Directories
-		if os.path.isdir(file):
-			all_files.remove(file)
+	allfiles_patterns = ["aws-keys","base64","cors","debug-pages","firebase","fw","go-functions","http-auth","ip","json-sec","meg-headers","php-curl","php-errors","php-serialized","php-sinks","php-sources","s3-buckets","sec","servers","strings","takeovers","upload-fields","urls","jsvar"]
 
-	try:
-		remove_patterns = ["","ip","sec","jsvar","php-errors","servers","upload-fields","urls","meg-headers","http-auth","debug-pages","fw","php-sinks","json-sec","s3-buckets","urls"]
-		for item in remove_patterns:
-			gf_list.remove(item)
-	except:
-		pass
-
-	pattern_with_one_process = ["ip","sec","jsvar","php-errors","servers","upload-fields","urls","meg-headers","http-auth","debug-pages","fw","php-sinks","json-sec","s3-buckets","urls"]   ###Patterns that display result from all file by default
-
-	for pattern in pattern_with_one_process:
-		try:
-			with open("../gf_results/summary.txt","a") as summary_file:
-				with open(f"../gf_results/{pattern}","a") as result_file:
-
-					print(colored(pattern,"yellow")+"  --> "+colored("*","cyan"),end="\r"),
-					sys.stdout.flush()
-					cmd = f"cat * | gf {pattern}"
-					output = (subprocess.check_output(cmd,shell=True)).decode()
-
-					if len(output)>1:
-						print(colored(pattern,"yellow")+"  --> "+colored(f"Found(Check {pattern} File)","cyan")+"                                                                 ")
-						summary_file.write(f"{pattern}  --> *\n")
-						result_file.write(output)
-
-						with open("../gf_results/logs.txt","a") as logFile:
-							logFile.write(f"{pattern} = Check Manually\n")
-
-		except KeyboardInterrupt:
-			ans = input(colored("\n\nKeyboard Interrupt Detected. Confirm Exit(y/n): ","red"))
-			if ans.lower()=="yes" or ans.lower()=="y":
-				print(colored("Output saved to gf_results","green"))
-				shutil.move("../gf_results","./")
-				exit(0)
-			else:
-				continue
-		except TypeError:
-			pass
-		except UnicodeDecodeError:
-			pass
-
-		##For other Patterns
-	for pattern in gf_list:
-
+	for pattern in allfiles_patterns:
 		try:
 			total_found = 0
 
-			with open("../gf_results/summary.txt","a") as summary_file:
-				with open(f"../gf_results/{pattern}","a") as result_file:
+			with open("gf_results/summary.txt","a") as summary_file:
+				with open(f"gf_results/{pattern}","a") as result_file:
 					for file in all_files:
-						print(colored(pattern,"yellow")+"  --> "+colored(file,"cyan"),end="\r"),
+						print(colored(pattern,"yellow")+"  --> "+colored(f"{file}                                              ","cyan"),end="\r"),
 						sys.stdout.flush()
 
-						cmd = f"cat {file} | gf {pattern}"
+						cmd = f"gf {pattern} {file}"
 						output = (subprocess.check_output(cmd,shell=True)).decode()
 						if len(output)>1:
 							total_found +=1
-							print(colored(pattern,"yellow")+"  --> "+colored(file,"cyan")+"                                                                 ")
+							result_file.write(output)
 							summary_file.write(f"{pattern}  --> {file}\n")
-							result_file.write(f"FILE007: {file}\n{output}\n---------------------------------------------------------------\n")
 
 					if total_found==0:
-						os.remove(f"../gf_results/{pattern}")
+						os.remove(f"gf_results/{pattern}")
 					else:
-						with open("../gf_results/logs.txt","a") as logFile:
+						print(colored(pattern,"yellow")+"  --> "+colored(f"{file} ","cyan")+"                                                                 ")
+						with open("gf_results/logs.txt","a") as logFile:
 							logFile.write(f"{pattern} = {total_found}\n")
 
 		except KeyboardInterrupt:
 			ans = input(colored("\n\nKeyboard Interrupt Detected. Confirm Exit(y/n): ","red"))
 			if ans.lower()=="yes" or ans.lower()=="y":
 				print(colored("Output saved to gf_results","green"))
-				shutil.move("../gf_results","./")
 				exit(0)
 			else:
 				continue
@@ -136,13 +91,62 @@ def gfauto():
 
 	print("                                                                                                            ")
 	print(colored("Output saved to gf_results","green"))
-	shutil.move("../gf_results","./")
 
+def gf_url_files(filename):
+
+	try:
+		os.mkdir("gf_results")
+	except FileExistsError:
+		print(colored("[-] gf_results Directory Already Exists","red"))
+		exit(0)
+
+	files = filename.split(",")
+	urlfile_patterns = ["debug_logic","idor","img-traversal","interestingEXT","interestingparams","lfi","rce","redirect","sqli","ssrf","ssti","xss","base64"]
+#	Not_added = ["interestingsubs"]
+
+	for pattern in urlfile_patterns:
+		for file in files:
+			try:
+				print(colored(pattern,"yellow")+"  --> "+colored("Analyzing","cyan"),end="\r"),
+				sys.stdout.flush()
+				cmd = f"gf {pattern} {file}"
+				output = (subprocess.check_output(cmd,shell=True)).decode()
+				if len(output)>1:
+					with open(f"gf_results/{pattern}","a") as result_file:
+						result_file.write(output)
+					with open("gf_results/summary.txt","a") as summary_file:
+						summary_file.write(f"{pattern}  --> {file}\n")
+					print(colored(pattern,"yellow")+"  --> "+colored(f"{file} ","cyan")+"                                                                 ")
+
+			except KeyboardInterrupt:
+				ans = input(colored("\n\nKeyboard Interrupt Detected. Confirm Exit(y/n): ","red"))
+				if ans.lower()=="yes" or ans.lower()=="y":
+					print(colored("Output saved to gf_results","green"))
+					exit(0)
+				else:
+					continue
+			except TypeError:
+				pass
+			except UnicodeDecodeError:
+				pass
+
+	print("                                                                                                            ")
+	print(colored("Output saved to gf_results","green"))
 
 def main():
 	banner()
-	get_arguments()
-	gfauto()	
+	args = get_arguments()
+
+	if args.all_files:
+		gf_all_files()
+
+	elif args.url_files:
+		file = args.url_files
+		gf_url_files(file)   
+
+	else:
+		print(colored("[-]Please Specify Either -u or -a","red"))
+		exit(0)
 
 
 main()
